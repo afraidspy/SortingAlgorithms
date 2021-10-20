ad996cc4857e308ba3e18b015344ef6cbfce3f08
Jessica San <afraidspy@gmail.com>
2021-10-20 14:46:35 -0500

    Se agrega quicksort y mergesort

--- Main.py
+++ Main.py
@@ -10,31 +10,48 @@ from OrdenamientoClass import Ordenamiento
 
 def genera_aleatorios():
     data = []
-    for i in range(10):
+    for i in range(1000):
         data.append(random.randint(1,10000000))
     
     return data
 
 #data = [1,5,4,2,0,0,9]
-data = [3,7,11,2,9,1,8,5,10,6,4]
-#data = genera_aleatorios()
+#data = [3,7,11,2,9,1,8,5,10,6,4]
+data = genera_aleatorios()
 ordenamiento = Ordenamiento()
-
-#print("Burbuja normal...")
-#ordenamiento.burbuja(data);
-
+print(data)
+#data = [1639672, 9080605, 7805992, 7530815, 292783, 8622560, 9751812, 8452579, 9321389, 281230, 1394183, 1483328, 3969962, 1988574, 5700117, 6189447, 4655291, 1422682, 9646675, 8670795, 7616764, 2440641, 210415, 5112990, 5347165, 5708287, 3824783, 6006026, 6903846, 749178, 9043323, 2868956, 7217003, 4004431, 1093061, 4318659, 3742505, 9092739, 4018046, 7257626, 9747026, 3019513, 518699, 7449038, 7535701, 782594, 1681710, 9182579, 7715822, 1523311, 25467, 4205725, 8068373, 4711235, 6664005, 4557819, 9557868, 7039034, 8235154, 4900164, 1593077, 7105079, 3584528, 5875968, 6193030, 9473556, 2897806, 9321419, 1786810, 8467737, 6456655, 17062, 8678988, 4433043, 2333707, 3218550, 6630543, 7161579, 8350566, 3321079, 1717451, 9360084, 5206476, 8296190, 1744200, 2411779, 7864320, 2406878, 4048925, 4121151, 8901654, 9205112, 9822295, 1336000, 1969238, 6810333, 4831283, 7845687, 5349172, 621829]
+print("Burbuja normal...")
+tiempo_inicio = time.time()
+ordenamiento.burbuja(data);
+tiempo_fin = time.time()-tiempo_inicio
+print("Burbuja normal: ", tiempo_fin)
 #data = [1,2,3,4,5,6]
-
-##print("Burbuja mejorado...")
-#ordenamiento.burbuja_mejorado(data)
-#print("Selección...")
-data = [7,6,2,9,3,1,8,4,5]
-#ordenamiento.seleccion(data)
-#print("Insercion")
-#ordenamiento.insercion(data)
-data = [79,21,15,99,88,65,75,85,76,46,84,24]
+print("Burbuja mejorado...")
+ordenamiento.burbuja_mejorado(data)
+#data = [7,6,2,9,3,1,8,4,5]
+tiempo_inicio = time.time()
+ordenamiento.seleccion(data)
+tiempo_fin = time.time()-tiempo_inicio
+print("Burbuja mejorado: ", tiempo_fin)
+print("Insercion")
+tiempo_inicio = time.time()
+ordenamiento.insercion(data)
+tiempo_fin = time.time()-tiempo_inicio
+print("Inserción: ", tiempo_fin)
+#data = [79,21,15,99,88,65,75,85,76,46,84,24]
 #data = [30,50,12,15,45]
-#data = ["jess","aana","pedro"]
+#data = ["jess","aana","pedro","salu","a"]
+print("Quicksort")
+tiempo_inicio = time.time()
 ordenamiento.quick_sort(data)
+tiempo_fin = time.time()-tiempo_inicio
+print("Quicksort: ", tiempo_fin)
+print("Mergesort")
+tiempo_inicio = time.time()
+ordenamiento.merge_sort(data)
+tiempo_fin = time.time()-tiempo_inicio
+print("Mergesort: ", tiempo_fin)
+
 
 
--- OrdenamientoClass.py
+++ OrdenamientoClass.py
@@ -11,24 +11,24 @@ class Ordenamiento(OrdenableAbstractClass):
     #https://docs.python.org/3/library/stdtypes.html#typesseq-range
     
     def burbuja(self,elementos):
-        self.imprimir(elementos)
+        #self.imprimir(elementos)
         for i in range(0,len(elementos),1):
-            print('\nIteracion: ' , i)
+           # print('\nIteracion: ' , i)
             for j in range(0,len(elementos)-1,1):#(O(n))
                 if(elementos[j] > elementos[j+1]):#O(1)
                     aux = elementos[j]
                     elementos[j] = elementos[j + 1]
                     elementos[j + 1] = aux
-                self.imprimir(elementos)
-        self.imprimir(elementos)
+                #self.imprimir(elementos)
+        #self.imprimir(elementos)
                 
             
     
     def burbuja_mejorado(self,elementos):
-        self.imprimir(elementos)
+        #self.imprimir(elementos)
         esta_ordenado = False;
         for i in range(0,len(elementos),1):
-            print("índice: " , i)
+            #print("índice: " , i)
             if (not esta_ordenado):
                 esta_ordenado = True
                 for j in range(0,len(elementos)-i-1):
@@ -37,15 +37,15 @@ class Ordenamiento(OrdenableAbstractClass):
                         aux = elementos[j]
                         elementos[j] = elementos[j + 1]
                         elementos[j + 1] = aux
-                    self.imprimir(elementos)
+                    #self.imprimir(elementos)
             else:
-                print("Sal......")
+                #print("Sal......")
                 break
             
-        self.imprimir(elementos)
+        #self.imprimir(elementos)
     
     def seleccion(self,elementos):
-        self.imprimir(elementos)
+        #self.imprimir(elementos)
         for i in range(0,len(elementos)):
             minimo = i
             for j in range(i+1, len(elementos),1):
@@ -54,25 +54,59 @@ class Ordenamiento(OrdenableAbstractClass):
             aux = elementos[i]
             elementos[i] = elementos[minimo]
             elementos[minimo] = aux
-            self.imprimir(elementos)
+            #self.imprimir(elementos)
 
         
     def insercion(self,elementos):
         for i in range(0,len(elementos)):
             aux = elementos[i]
             for j in range(i-1, -1,-1):
-                self.imprimir(elementos)
+               # print("\ni={}j={} ".format( i,j))
+                #self.imprimir(elementos)
                 if (elementos[j]>aux):
                     elementos[j+1] = elementos[j]
                     elementos[j] = aux
 
-        self.imprimir(elementos)
+        #self.imprimir(elementos)
     
     def merge_sort(self,elementos):
-        pass
-    
+        #print(elementos)
+        if(len(elementos) <= 1):
+            return elementos
+        izq=[];
+        der=[];
+        indice_medio=len(elementos)//2
+        for i in range(0,indice_medio):
+            izq.append(elementos[i]);
+        for i in range(indice_medio,len(elementos)):
+            der.append(elementos[i]);
+        izq=self.merge_sort(izq)
+        der=self.merge_sort(der)
+        return self.merge_aux(izq,der)
+ 
+    def merge_aux(self,izq,der):
+        lista =[];
+        i=0;
+        j=0;
+        while(i < len(izq) and j < len(der)):
+            if(izq[i] <= der[j]):
+                lista.append(izq[i])
+                i=i+1;
+            else:
+                lista.append(der[j])
+                j=j+1;
+
+        while(i < len(izq)):
+            lista.append(izq[i])
+            i=i+1;
+        while(j < len(der)):
+            lista.append(der[j])
+            j=j+1;
+
+        return lista
+   
     def quick_sort(self,elementos):
-        self.imprimir(elementos)
+        #self.imprimir(elementos)
         self.quick_sort_aux(elementos, 0, len(elementos)-1)
         
     def quick_sort_aux(self,elementos, left, right):
@@ -86,13 +120,13 @@ class Ordenamiento(OrdenableAbstractClass):
         i = left
         j = right
         pivot = elementos[int((left+right)/2)]
-        print('\nLeft: {} Right: {} Índice: {} Pivote: {}'.format(left,right,int((left+right)/2),pivot))
+        #print('\nLeft: {} Right: {} Índice: {} Pivote: {}'.format(left,right,int((left+right)/2),pivot))
         while(i <= j):
             while(elementos[i]<pivot):
                 i+=1
             while(elementos[j]>pivot):
                 j-=1
-            print("\ni= {} j= {}".format(i,j))
+           # print("\ni= {} j= {}".format(i,j))
             if(i<=j):
                 tmp = elementos[i];
                 elementos[i] = elementos[j];
@@ -100,7 +134,7 @@ class Ordenamiento(OrdenableAbstractClass):
                 i+=1
                 j-=1
             
-            self.imprimir(elementos)
+            #self.imprimir(elementos)
         
         return i
             
